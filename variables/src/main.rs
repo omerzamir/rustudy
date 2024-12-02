a9d755af49c4829aa6631871285b2842e3fb3488
omer zamir <zamir98@gmail.com>
2024-12-02 11:00:26 +0200

    more basics
    Signed-off-by: omer zamir <zamir98@gmail.com>

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -66,7 +66,9 @@ fn main4() {
     let _first = a[0];
 }
 
-fn main5() {
+fn _main5() {
+    println!("main5:");
+
     let a = [1, 2, 3, 4, 5];
 
     println!("Please enter an array index.");
@@ -100,5 +102,122 @@ fn main() {
     main2();
     main3();
     main4();
-    main5();
+    // main5();
+    // main6();
+    another_function(2);
+    print_labeled_measurement(6, 'f');
+    println!("Five is: {}", five());
+    println!("Six is: {}", plus_one(five()));
+
+    main7();
+    main8();
+    main9();
+}
+
+fn another_function(x: i32) {
+    println!("another_function:");
+
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("print_labeled_measurement:");
+
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn _main6() {
+    println!("main6");
+
+    println!("Please enter a number:");
+
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    let number: usize = number
+        .trim()
+        .parse()
+        .expect("The number entered was not a number");
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+fn main7() {
+    println!("main7");
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn main8() {
+    println!("main8");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn main9() {
+    println!("main9");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
