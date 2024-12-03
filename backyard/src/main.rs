d4309c74c357410e86c3fe3fac695adc280e04c5
omer zamir <zamir98@gmail.com>
2024-12-03 14:44:03 +0200

    moduling
    Signed-off-by: omer zamir <zamir98@gmail.com>

--- backyard/src/main.rs
+++ backyard/src/main.rs
@@ -1,8 +1,15 @@
 mod garden;
+mod omer;
 
 use crate::garden::vegetables::Asparagus;
 
 fn main() {
     let plant = Asparagus {};
     println!("I'm growing {plant:?}!");
+}
+
+pub use crate::omer::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
